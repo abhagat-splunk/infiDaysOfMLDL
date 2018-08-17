6663cdbe2821c6799f5ac16d7ec4fd6abb00dbb0
Ankit Bhagat <acb693@nyu.edu>
2018-08-17 00:29:58 -0400

    Adam Optimization Comment

--- LearningPytorchWithExamples/AdamOptim.py
+++ LearningPytorchWithExamples/AdamOptim.py
@@ -14,6 +14,11 @@ model = torch.nn.Sequential(
 loss_fn = torch.nn.MSELoss(reduction = 'sum')
 
 learning_rate = 1e-4
+
+# Use the optim package to define an Optimizer that will update the weights of
+# the model for us. Here we will use Adam; the optim package contains many other
+# optimization algoriths. The first argument to the Adam constructor tells the
+# optimizer which Tensors it should update.
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
